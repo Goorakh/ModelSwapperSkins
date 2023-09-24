deb898c765147612fd6dd5f02efe9e9514eaa7c2
a22petoh <petter.ohlen@gmail.com>
2023-09-24 19:59:28 +0200

    Add Void Fiend bone offsets

--- ModelSwapperSkins/BoneMapping/InitializerRules/BoneInitializerRules_VoidFiend.cs
+++ ModelSwapperSkins/BoneMapping/InitializerRules/BoneInitializerRules_VoidFiend.cs
@@ -13,54 +13,118 @@ namespace ModelSwapperSkins.BoneMapping.InitializerRules
         protected override BoneInfo getBoneInfo(Transform modelTransform, Transform potentialBoneTransform)
         {
             BoneInfo boneInfo = base.getBoneInfo(modelTransform, potentialBoneTransform);
-            if (boneInfo.Type != BoneType.None)
-                return boneInfo;
-
-            switch (potentialBoneTransform.name)
+            if (boneInfo.Type == BoneType.None)
             {
-                case "Hand":
-                    return new BoneInfo(BoneType.HandL);
-                case "Index1":
-                    return new BoneInfo(BoneType.IndexFinger1L);
-                case "Index2":
-                    return new BoneInfo(BoneType.IndexFinger2L);
-                case "Index3":
-                    return new BoneInfo(BoneType.IndexFinger3L);
-                case "IndexEnd":
-                    return new BoneInfo(BoneType.IndexFinger3L_end);
-                case "Middle1":
-                    return new BoneInfo(BoneType.MiddleFinger1L);
-                case "Middle2":
-                    return new BoneInfo(BoneType.MiddleFinger2L);
-                case "Middle3":
-                    return new BoneInfo(BoneType.MiddleFinger3L);
-                case "MiddleEnd":
-                    return new BoneInfo(BoneType.MiddleFinger3L_end);
-                case "Pinky1":
-                    return new BoneInfo(BoneType.PinkyFinger1L);
-                case "Pinky2":
-                    return new BoneInfo(BoneType.PinkyFinger2L);
-                case "PinkyEnd":
-                    return new BoneInfo(BoneType.PinkyFinger3L);
-                case "PinkyEnd_end":
-                    return new BoneInfo(BoneType.PinkyFinger3L_end);
-                case "Ring1":
-                    return new BoneInfo(BoneType.RingFinger1L);
-                case "Ring2":
-                    return new BoneInfo(BoneType.RingFinger2L);
-                case "Ring3":
-                    return new BoneInfo(BoneType.RingFinger3L);
-                case "RingEnd":
-                    return new BoneInfo(BoneType.RingFinger3L_end);
-                case "Thumb1":
-                    return new BoneInfo(BoneType.Thumb1L);
-                case "Thumb2":
-                    return new BoneInfo(BoneType.Thumb2L);
-                case "ThumbEnd":
-                    return new BoneInfo(BoneType.Thumb2L_end);
-                default:
-                    return BoneInfo.None;
+                switch (potentialBoneTransform.name)
+                {
+                    case "Hand":
+                        boneInfo = new BoneInfo(BoneType.HandL);
+                        break;
+                    case "Index1":
+                        boneInfo = new BoneInfo(BoneType.IndexFinger1L);
+                        break;
+                    case "Index2":
+                        boneInfo = new BoneInfo(BoneType.IndexFinger2L);
+                        break;
+                    case "Index3":
+                        boneInfo = new BoneInfo(BoneType.IndexFinger3L);
+                        break;
+                    case "IndexEnd":
+                        boneInfo = new BoneInfo(BoneType.IndexFinger3L_end);
+                        break;
+                    case "Middle1":
+                        boneInfo = new BoneInfo(BoneType.MiddleFinger1L);
+                        break;
+                    case "Middle2":
+                        boneInfo = new BoneInfo(BoneType.MiddleFinger2L);
+                        break;
+                    case "Middle3":
+                        boneInfo = new BoneInfo(BoneType.MiddleFinger3L);
+                        break;
+                    case "MiddleEnd":
+                        boneInfo = new BoneInfo(BoneType.MiddleFinger3L_end);
+                        break;
+                    case "Pinky1":
+                        boneInfo = new BoneInfo(BoneType.PinkyFinger1L);
+                        break;
+                    case "Pinky2":
+                        boneInfo = new BoneInfo(BoneType.PinkyFinger2L);
+                        break;
+                    case "PinkyEnd":
+                        boneInfo = new BoneInfo(BoneType.PinkyFinger3L);
+                        break;
+                    case "PinkyEnd_end":
+                        boneInfo = new BoneInfo(BoneType.PinkyFinger3L_end);
+                        break;
+                    case "Ring1":
+                        boneInfo = new BoneInfo(BoneType.RingFinger1L);
+                        break;
+                    case "Ring2":
+                        boneInfo = new BoneInfo(BoneType.RingFinger2L);
+                        break;
+                    case "Ring3":
+                        boneInfo = new BoneInfo(BoneType.RingFinger3L);
+                        break;
+                    case "RingEnd":
+                        boneInfo = new BoneInfo(BoneType.RingFinger3L_end);
+                        break;
+                    case "Thumb1":
+                        boneInfo = new BoneInfo(BoneType.Thumb1L);
+                        break;
+                    case "Thumb2":
+                        boneInfo = new BoneInfo(BoneType.Thumb2L);
+                        break;
+                    case "ThumbEnd":
+                        boneInfo = new BoneInfo(BoneType.Thumb2L_end);
+                        break;
+                }
             }
+
+            switch (boneInfo.Type)
+            {
+                case BoneType.Pelvis:
+                    boneInfo.RotationOffset = Quaternion.Euler(0f, 270f, 0f);
+                    break;
+
+                case BoneType.LegUpperL:
+                    boneInfo.RotationOffset = Quaternion.Euler(0f, 90f, 0f);
+                    break;
+                case BoneType.LegLowerL:
+                    boneInfo.RotationOffset = Quaternion.Euler(0f, 270f, 0f);
+                    break;
+                case BoneType.FootL:
+                    boneInfo.RotationOffset = Quaternion.Euler(0f, 270f, 0f);
+                    break;
+                case BoneType.Toe1L:
+                    boneInfo.RotationOffset = Quaternion.Euler(0f, 90f, 0f);
+                    break;
+
+                case BoneType.LegUpperR:
+                    boneInfo.RotationOffset = Quaternion.Euler(0f, 270f, 0f);
+                    break;
+                case BoneType.LegLowerR:
+                    boneInfo.RotationOffset = Quaternion.Euler(0f, 90f, 0f);
+                    break;
+                case BoneType.FootR:
+                    boneInfo.RotationOffset = Quaternion.Euler(0f, 90f, 0f);
+                    break;
+                case BoneType.Toe1R:
+                    boneInfo.RotationOffset = Quaternion.Euler(0f, 270f, 0f);
+                    break;
+
+                case BoneType.ArmLowerL:
+                    boneInfo.RotationOffset = Quaternion.Euler(0f, 270f, 0f);
+                    break;
+                case BoneType.HandL:
+                    boneInfo.RotationOffset = Quaternion.Euler(0f, 90f, 0f);
+                    break;
+
+                case BoneType.ArmUpperR:
+                    boneInfo.RotationOffset = Quaternion.Euler(0f, 180f, 0f);
+                    break;
+            }
+
+            return boneInfo;
         }
     }
 }
