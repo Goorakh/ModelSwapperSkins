911edc51cfc1af15f6f8de16b808dcdf49966475
a22petoh <petter.ohlen@gmail.com>
2023-10-01 06:36:12 +0200

    Scav bone offsets

--- ModelSwapperSkins/BoneMapping/BoneInitializer.cs
+++ ModelSwapperSkins/BoneMapping/BoneInitializer.cs
@@ -59,6 +59,7 @@ namespace ModelSwapperSkins.BoneMapping
             AddCustomBoneInitializerRules(BoneInitializerRules_Mage.Instance);
             AddCustomBoneInitializerRules(BoneInitializerRules_Parent.Instance);
             AddCustomBoneInitializerRules(BoneInitializerRules_Railgunner.Instance);
+            AddCustomBoneInitializerRules(BoneInitializerRules_Scav.Instance);
 
             foreach (CharacterBody body in BodyCatalog.allBodyPrefabBodyBodyComponents)
             {
---
+++ ModelSwapperSkins/BoneMapping/InitializerRules/BoneInitializerRules_Scav.cs
@@ -0,0 +1,48 @@
+﻿using RoR2;
+using UnityEngine;
+
+namespace ModelSwapperSkins.BoneMapping.InitializerRules
+{
+    public class BoneInitializerRules_Scav : BoneInitializerRules_AutoName
+    {
+        public static new readonly BoneInitializerRules_Scav Instance = new BoneInitializerRules_Scav();
+
+        protected BoneInitializerRules_Scav() : base()
+        {
+        }
+
+        public override bool AppliesTo(BodyIndex bodyIndex)
+        {
+            return bodyIndex == BodyCatalog.FindBodyIndex("ScavBody") ||
+                   bodyIndex == BodyCatalog.FindBodyIndex("ScavLunar1Body") ||
+                   bodyIndex == BodyCatalog.FindBodyIndex("ScavLunar2Body") ||
+                   bodyIndex == BodyCatalog.FindBodyIndex("ScavLunar3Body") ||
+                   bodyIndex == BodyCatalog.FindBodyIndex("ScavLunar4Body");
+        }
+
+        protected override BoneInfo getBoneInfo(Transform modelTransform, Transform potentialBoneTransform)
+        {
+            BoneInfo bone = base.getBoneInfo(modelTransform, potentialBoneTransform);
+            if (bone.Type != BoneType.None)
+            {
+                switch (bone.Type)
+                {
+                    case BoneType.Stomach:
+                    case BoneType.Chest:
+                    case BoneType.ShoulderL:
+                    case BoneType.ArmUpperL:
+                    case BoneType.ArmLowerL:
+                    case BoneType.ShoulderR:
+                    case BoneType.ArmUpperR:
+                    case BoneType.ArmLowerR:
+                        bone.RotationOffset *= Quaternion.Euler(0f, 180f, 0f);
+                        break;
+                }
+
+                return bone;
+            }
+
+            return bone;
+        }
+    }
+}
