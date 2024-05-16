88d7c43da18fcf96075950c573a686d077caeaa0
Gorakh <petter.ohlen@gmail.com>
2024-05-16 19:54:10 +0200

    Add default skin to all bodies

---
+++ ModelSwapperSkins/Utils/Comparers/GameObjectActivationObjectComparer.cs
@@ -0,0 +1,24 @@
+﻿using RoR2;
+using System.Collections.Generic;
+
+namespace ModelSwapperSkins.Utils.Comparers
+{
+    public sealed class GameObjectActivationObjectComparer : IEqualityComparer<SkinDef.GameObjectActivation>
+    {
+        public static GameObjectActivationObjectComparer Instance { get; } = new GameObjectActivationObjectComparer();
+
+        GameObjectActivationObjectComparer()
+        {
+        }
+
+        public bool Equals(SkinDef.GameObjectActivation x, SkinDef.GameObjectActivation y)
+        {
+            return x.gameObject == y.gameObject;
+        }
+
+        public int GetHashCode(SkinDef.GameObjectActivation obj)
+        {
+            return obj.gameObject.GetHashCode();
+        }
+    }
+}
