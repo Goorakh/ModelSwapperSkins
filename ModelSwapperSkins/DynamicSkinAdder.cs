b59ad8c6219dd77d5f3e98637594dae3dc3ab462
Gorakh <petter.ohlen@gmail.com>
2024-05-16 19:54:10 +0200

    Add default skin to all bodies

--- ModelSwapperSkins/DynamicSkinAdder.cs
+++ ModelSwapperSkins/DynamicSkinAdder.cs
@@ -1,4 +1,5 @@
-﻿using ModelSwapperSkins.BoneMapping;
+﻿using HG;
+using ModelSwapperSkins.BoneMapping;
 using ModelSwapperSkins.ModelParts;
 using ModelSwapperSkins.Utils;
 using RoR2;
@@ -28,6 +29,134 @@ namespace ModelSwapperSkins
             }
 
             On.RoR2.SkinDef.Bake += SkinDef_Bake;
+
+            foreach (CharacterBody body in BodyCatalog.allBodyPrefabBodyBodyComponents)
+            {
+                SurvivorDef survivorDef = SurvivorCatalog.FindSurvivorDefFromBody(body.gameObject);
+
+                ModelLocator modelLocator = body.GetComponent<ModelLocator>();
+                if (!modelLocator)
+                    continue;
+
+                Transform modelTransform = modelLocator.modelTransform;
+                if (!modelTransform)
+                    continue;
+
+                CharacterModel characterModel = modelTransform.GetComponent<CharacterModel>();
+                if (!characterModel)
+                    continue;
+
+                ModelPart[] modelParts;
+                if (modelTransform.TryGetComponent(out ModelPartsProvider modelPartsProvider))
+                {
+                    modelParts = modelPartsProvider.Parts;
+                }
+                else
+                {
+                    modelParts = [];
+                }
+
+                if (modelTransform.TryGetComponent(out ModelSkinController modelSkinController))
+                {
+                    foreach (SkinDef skin in modelSkinController.skins)
+                    {
+                        List<SkinDef.GameObjectActivation> gameObjectActivations = new List<SkinDef.GameObjectActivation>(skin.gameObjectActivations);
+
+                        bool gameObjectActivationsChanged = false;
+
+                        foreach (ModelPart modelPart in modelParts)
+                        {
+                            bool partAlreadyExists = false;
+                            foreach (SkinDef.GameObjectActivation gameObjectActivation in gameObjectActivations)
+                            {
+                                string activationPath = Util.BuildPrefabTransformPath(skin.rootObject.transform, gameObjectActivation.gameObject.transform);
+                                if (activationPath == modelPart.Path)
+                                {
+                                    partAlreadyExists = true;
+                                    break;
+                                }
+                            }
+
+                            if (!partAlreadyExists)
+                            {
+                                Transform partTransform = skin.rootObject.transform.Find(modelPart.Path);
+                                if (partTransform)
+                                {
+                                    gameObjectActivations.Add(new SkinDef.GameObjectActivation
+                                    {
+                                        gameObject = partTransform.gameObject,
+                                        shouldActivate = true
+                                    });
+
+                                    gameObjectActivationsChanged = true;
+                                }
+                            }
+                        }
+
+                        if (gameObjectActivationsChanged)
+                        {
+                            skin.gameObjectActivations = gameObjectActivations.ToArray();
+
+                            // Force re-baking
+#pragma warning disable Publicizer001 // Accessing a member that was not originally public
+                            skin.runtimeSkin = null;
+#pragma warning restore Publicizer001 // Accessing a member that was not originally public
+                        }
+                    }
+                }
+                else
+                {
+                    modelSkinController = modelTransform.gameObject.AddComponent<ModelSkinController>();
+
+                    _skinBakeDisabled = true;
+                    SkinDef defaultSkin = ScriptableObject.CreateInstance<SkinDef>();
+                    _skinBakeDisabled = false;
+
+                    defaultSkin.name = $"skin{body.name}Default";
+
+                    defaultSkin.rootObject = modelTransform.gameObject;
+
+                    defaultSkin.baseSkins = [];
+
+                    defaultSkin.rendererInfos = characterModel.baseRendererInfos;
+
+                    defaultSkin.gameObjectActivations = Array.ConvertAll(modelParts, p => new SkinDef.GameObjectActivation
+                    {
+                        gameObject = p.Transform.gameObject,
+                        shouldActivate = true
+                    });
+
+                    defaultSkin.meshReplacements = [];
+                    defaultSkin.projectileGhostReplacements = [];
+                    defaultSkin.minionSkinReplacements = [];
+
+                    modelSkinController.skins = [defaultSkin];
+
+                    BodyIndex bodyIndex = body.bodyIndex;
+                    if (bodyIndex != BodyIndex.None)
+                    {
+#pragma warning disable Publicizer001 // Accessing a member that was not originally public
+                        SkinDef[][] skins = BodyCatalog.skins;
+#pragma warning restore Publicizer001 // Accessing a member that was not originally public
+                        if ((int)bodyIndex < skins.Length)
+                        {
+                            ArrayUtils.ArrayAppend(ref skins[(int)bodyIndex], defaultSkin);
+                        }
+                    }
+                }
+
+                if (survivorDef && survivorDef.displayPrefab)
+                {
+                    CharacterModel survivorDisplayCharacterModel = survivorDef.displayPrefab.GetComponentInChildren<CharacterModel>();
+                    if (survivorDisplayCharacterModel)
+                    {
+                        if (!survivorDisplayCharacterModel.TryGetComponent(out ModelSkinController survivorDisplaySkinController))
+                            survivorDisplaySkinController = survivorDisplayCharacterModel.gameObject.AddComponent<ModelSkinController>();
+
+                        survivorDisplaySkinController.skins = modelSkinController.skins;
+                    }
+                }
+            }
 
             foreach (CharacterBody body in BodyCatalog.allBodyPrefabBodyBodyComponents)
             {
